89b66bcb56d62f4aed6eed7557e610435a931f7d
lessandroFreitas <lessandrof@gmail.com>
2025-10-25 13:04:47 -0300

    exercicio 2025-10-17

---
+++ media.js
@@ -0,0 +1,19 @@
+let nome = prompt("Digite o nome do aluno:");
+let nota1 = parseFloat(prompt("Digite a primeira nota:"));
+let nota2 = parseFloat(prompt("Digite a segunda nota:"));
+let nota3 = parseFloat(prompt("Digite a terceira nota:"));
+
+let media = (nota1 + nota2 + nota3) / 3;
+
+let situacao;
+if (media >= 7) {
+    situacao = "Aprovado!";
+} else if (media >= 5) {
+    situacao = "Em Recuperação!";
+} else {
+    situacao = "Reprovado!";
+}
+
+console.log(`Aluno: ${nome}`);
+console.log(`Média: ${media}`);
+console.log(`Situação: ${situacao}`);
